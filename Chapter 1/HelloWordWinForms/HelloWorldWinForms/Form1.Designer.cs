2521011577023f6fb19da969e7c6f29f8119bbe5
mguerrahahn <marcelogh@outlook.com>
2024-06-16 23:15:51 -0700

    Adding Code for Chapter 1

---
+++ Chapter 1/HelloWordWinForms/HelloWorldWinForms/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Book1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnGreet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(167, 122);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(379, 38);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnGreet
+            // 
+            this.btnGreet.Location = new System.Drawing.Point(577, 113);
+            this.btnGreet.Name = "btnGreet";
+            this.btnGreet.Size = new System.Drawing.Size(143, 55);
+            this.btnGreet.TabIndex = 2;
+            this.btnGreet.Text = "Greet";
+            this.btnGreet.UseVisualStyleBackColor = true;
+            this.btnGreet.Click += new System.EventHandler(this.btnGreet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 292);
+            this.Controls.Add(this.btnGreet);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnGreet;
+    }
+}
+
---
+++ Chapter 1/HelloWorldMAUI/HelloWorldMAUI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorldMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnGreetButtonClicked(object sender, EventArgs e)
+        {
+            GreetingLabel.Text = $"Hello {NameEntry.Text}";
+        }
+    }
+}

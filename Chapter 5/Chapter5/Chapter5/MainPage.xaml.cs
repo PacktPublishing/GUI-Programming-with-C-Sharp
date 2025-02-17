cd16233843530112d8ee27256bad648c72157faf
mguerrahahn <marcelo@letsusedata.com>
2025-02-17 14:13:09 -0800

    Chapter 5

---
+++ Chapter 5/Chapter5/Chapter5/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Chapter5
+{
+    public class Item
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+
+    public class ViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public ViewModel()
+        {
+            Items = new ObservableCollection<Item>
+            {
+                new Item { Name = "Item 1", Description = "Description 1" },
+                new Item { Name = "Item 2", Description = "Description 2" }
+            };
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel();
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            DisplayAlert("Button Clicked", "You clicked the button!", "OK");
+        }
+    }
+}

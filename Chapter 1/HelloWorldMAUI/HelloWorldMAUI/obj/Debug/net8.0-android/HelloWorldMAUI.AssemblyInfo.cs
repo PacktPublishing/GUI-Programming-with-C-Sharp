c3c42bbf42636d0b5b54f055e0d3d08196b1dcc1
mguerrahahn <marcelo@letsusedata.com>
2025-02-16 17:38:18 -0800

    Updating Chapter 1

--- Chapter 1/HelloWorldMAUI/HelloWorldMAUI/obj/Debug/net8.0-android/HelloWorldMAUI.AssemblyInfo.cs
+++ Chapter 1/HelloWorldMAUI/HelloWorldMAUI/obj/Debug/net8.0-android/HelloWorldMAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldMAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+285f857cb6c2b1b71ae288b77bac331a57a44acf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldMAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldMAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

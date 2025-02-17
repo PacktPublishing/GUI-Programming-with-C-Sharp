341cbb7dc434ecb7608d7eb3eed2c947fcdf29f5
mguerrahahn <marcelo@letsusedata.com>
2025-02-16 18:04:07 -0800

    Chapter 1 - Blazor

--- Chapter 1/HelloWorldBlazor/HelloWorldBlazor/obj/Debug/net8.0/HelloWorldBlazor.AssemblyInfo.cs
+++ Chapter 1/HelloWorldBlazor/HelloWorldBlazor/obj/Debug/net8.0/HelloWorldBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ac339e16f6d09a265e89f52ca141e48ab0d9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
